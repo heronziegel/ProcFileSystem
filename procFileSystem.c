c1c2f5b9af565d954b6b557b09ad0ded7da47c1f
heronziegel <tuo12440@temple.edu>
2023-09-20 13:54:43 -0400

    initial commit

---
+++ procFileSystem.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    // Week 1
+
+    // Create struct for flags, use getopt() to fill
+
+    // Create second struct for proc options
+
+    // Parse proc/PID/stat
+
+    // Parse proc/PID/statm
+
+    // Parse proc/PID/cmdline
+
+    exit(0);
+}
