63458321ad2a558e568a9054284b13a09b41cd57
heronziegel <tuo12440@temple.edu>
2023-09-20 14:06:39 -0400

    Added structs for flag arguments and procedure details

--- procFileSystem.c
+++ procFileSystem.c
@@ -6,8 +6,24 @@ int main(int argc, char *argv[]) {
     // Week 1
 
     // Create struct for flags, use getopt() to fill
+    struct flags {
+        int v_opt;
+        int p_opt;
+        int PID_opt;
+        int c_opt;
+        int U_opt;
+        int S_opt;
+        int s_opt;
+    };
 
     // Create second struct for proc options
+    struct proc {
+        int pid;
+        char state;
+        char cmd[4096];
+        unsigned int utime;
+        unsigned int vtime;
+    };
 
     // Parse proc/PID/stat
 
